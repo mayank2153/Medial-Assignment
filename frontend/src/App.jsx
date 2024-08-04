d679448ec72393070e5e515430f519116b13ba9e
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-05 01:19:29 +0530

    added homepage and postcard component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,31 @@
-export default function App() {
+import React from 'react';
+import { createBrowserRouter, RouterProvider, Outlet } from 'react-router-dom';
+import HomePage from './components/HomePage';
+
+
+function Layout() {
   return (
-   <div>
-      Medial Assignment
-   </div>
-  )
+    <div className="montserrat-medium">
+      <Outlet />
+    </div>
+  );
 }
+
+const appRouting = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+        {path: "/",
+        element: (
+            <HomePage />
+        ),}
+    ],
+  },
+]);
+
+function App() {
+  return <RouterProvider router={appRouting} />;
+}
+
+export default App;
