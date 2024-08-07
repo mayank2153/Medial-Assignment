845170c4efa5fa86b48829cd7b76e5e50a6802d8
mayank2153 <114002151+mayank2153@users.noreply.github.com>
2024-08-07 14:06:57 +0530

    "Rename PORT to port in backend/index.js"

--- backend/index.js
+++ backend/index.js
@@ -8,8 +8,8 @@ dotenv.config({
 });
 
 // Ensure PORT is loaded from environment variables
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
-  console.log('listening on port ' + PORT);
+app.listen(port, () => {
+  console.log('listening on port ' + port);
 });
